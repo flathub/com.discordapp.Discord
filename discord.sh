ed41b8108fcd230cc39813d463754111d5678f85
gasinvein <gasinvein@gmail.com>
2019-05-17 12:03:53 +0300

    Use xdg-run/app/$id dir instead of custom one

--- discord.sh
+++ discord.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+FLATPAK_ID=${FLATPAK_ID:-"com.discordapp.Discord"}
 socat $SOCAT_ARGS \
-    UNIX-LISTEN:$XDG_RUNTIME_DIR/discord/ipc-0,forever,fork \
+    UNIX-LISTEN:$XDG_RUNTIME_DIR/app/${FLATPAK_ID}/discord-ipc-0,forever,fork \
     UNIX-CONNECT:$XDG_RUNTIME_DIR/discord-ipc-0 \
     &
 socat_pid=$!
