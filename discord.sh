5fd294d592a023b00213ce8745235a8d73207c40
Alexander Koskovich <akoskovich@pm.me>
2024-01-19 20:49:19 -0500

    Remove all GPU performance optimizations from startup script
    Let's see how this goes, and if there are still issues then
    it's up to upstream Discord to resolve them.
    
    Test: Open Discord on RX 7900 XTX

--- discord.sh
+++ discord.sh
@@ -6,18 +6,11 @@ socat $SOCAT_ARGS \
     &
 socat_pid=$!
 
-FLAGS='--enable-gpu-rasterization --enable-zero-copy --enable-gpu-compositing --enable-native-gpu-memory-buffers --enable-oop-rasterization --enable-features=UseSkiaRenderer,WaylandWindowDecorations'
-
 WAYLAND_SOCKET=${WAYLAND_DISPLAY:-"wayland-0"}
 
 if [[ -e "$XDG_RUNTIME_DIR/${WAYLAND_SOCKET}" ]]
 then
-    FLAGS="$FLAGS --ozone-platform-hint=auto"
-fi
-
-if [[ $XDG_SESSION_TYPE == "wayland" ]] && [ -c /dev/nvidia0 ]
-then
-    FLAGS="$FLAGS --use-vulkan"
+    FLAGS="--enable-features=WaylandWindowDecorations --ozone-platform-hint=auto"
 fi
 
 disable-breaking-updates.py
